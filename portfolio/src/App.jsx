b7cf3b61e6ad60d9a72c96cdaddb51dec5d0019a
Jeffrey Scott <82685075+vader9911@users.noreply.github.com>
2024-04-08 19:26:17 -0400

    project setup

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './index.css'
+
+function App() {
+  return (
+    
+    <div className='h-screen flex bg-gray-950 justify-center place-items-center'>
+    <h1 className='text-9xl font-bold text-emerald-500 font-spaceGrotesk'>Welcome.</h1>
+     </div>
+  )
+}
+
+export default App
