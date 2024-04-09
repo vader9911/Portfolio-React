4b87c157fa86e6c8e2ebeb3d56867a7bfaf58d19
Jeffrey Scott <82685075+vader9911@users.noreply.github.com>
2024-04-08 21:33:34 -0400

    add projects logic for adding more projects from github and first project

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header'
 import About from './components/About';
-// import Projects from './components/Projects';
+import Projects from './components/Projects';
 // import Skills from './components/Skills';
 // import Contact from './components/Contact';
 import Footer from './components/Footer';
@@ -11,11 +11,11 @@ import './index.css'
 const App = () => {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen bg-slate-800 font-spaceGrotesk">
+      <div className="flex flex-col min-h-screen bg-slate-800 ">
         <Header />
         <main className="flex-grow">
           <About />
-          {/* <Projects /> */}
+          <Projects />
           {/* <Skills /> */}
           {/* <Contact /> */}
         </main>
---
+++ portfolio/src/components/Projects.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const projects = [
+  {
+    id: 1,
+    name: "Spellbinder",
+    description: "Spellbinder is an web-app to view and collect Magic the Gathering cards. It allows you to easily search and add cards to your collection.",
+    image: "src/assets/Spellbinder.jpg" ,
+    repoUrl: "https://github.com/vader9911/Spellbinder",
+  },
+]
+
+export default function Projects() {
+  return (
+    <section id="projects" className="min-h-screen p-2 text-emerald-400 bg-slate-800">
+      <div className="container mx-auto">
+        <h2 className="text-9xl md:text-4xl font-bold mb-6 underline">Projects</h2>
+        <div className="grid grid-cols-1 md:grid-rows-3 gap-4 w-full">
+          {projects.map((project) => (
+            <div key={project.id} className="rounded overflow-hidden shadow-lg bg-slate-700 hover:bg-slate-600  transition-colors duration-300">
+              <a href={project.repoUrl} target="_blank">
+                <img className="w-full" src={project.image} alt={project.name} />
+              </a>
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{project.name}</div>
+                <p className="text-gray-300 text-base">
+                  {project.description}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
