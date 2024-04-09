0c3ca5ccfa0e26140834e552d093cddab91c4a8a
Jeffrey Scott <82685075+vader9911@users.noreply.github.com>
2024-04-08 21:51:22 -0400

    fix file names

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header'
-import About from './components/About';
+import React from 'react'
+import { BrowserRouter as Router} from 'react-router-dom';
+import Header from './components/header';
+import About from './components/about';
 import Projects from './components/Projects';
 // import Skills from './components/Skills';
 // import Contact from './components/Contact';
